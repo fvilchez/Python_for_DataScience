51708aacb1c9628d5fc3edc6a472d91d1afcf140
Francisco Javier <franciscojaviervilcheztorralba@gmail.com>
2018-10-20 20:18:49 +0200

    Descomposicion de series temporales modelos aditivos y multiplicativos

--- Temporal Series/Descomposicion de una serie temporal/code.py
+++ Temporal Series/Descomposicion de una serie temporal/code.py
@@ -4,22 +4,23 @@
 #Python dispone de la función seasonal_decompose que nos permite descomponer
 #nuestra serie en sus respectivos componentes.
 
-from statsmodels import tsa
+import statsmodels.api as sm
 import pandas as pd
+from random import randrange
+import matplotlib.pyplot as pl
 
 #Realizamos una descomposición aditiva
-from random import randrange
-import matplotlib.pyplot as plt
+
 
 series = [i + randrange(10) for i in range(1,100)]
-result = tsa.seasonal.seasonal_decompose(series, model = 'additive', freq = 1)
+result = sm.tsa.seasonal_decompose(series, model = 'additive', freq = 1)
 result.plot()
 plt.show()
 
 #Realizamos una descomposición multiplicativa
 
 series = [i**2 + randrange(10) for i in range(1,100)]
-result = tsa.seasonal.seasonal_decompose(series, model = 'additive', freq = 1)
+result = sm.tsa.seasonal_decompose(series, model = 'additive', freq = 1)
 result.plot()
 plt.show()
 
@@ -27,6 +28,6 @@ plt.show()
 #Ejemplo con un dataset real
 series = pd.read_csv('airline-passengers.csv', header = 0, index_col = 0,
                      squeeze = True)
-result = tsa.seasonal.seasonal_decompose(series, model = 'multiplicative')
+result = sm.tsa.seasonal_decompose(series, model = 'multiplicative')
 result.plot()
 plt.show()
